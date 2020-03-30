d6e954bb48db48c3f7baf107aebb51dc08f4e5ec
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-03-30 11:41:46 +0530

    started

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  let count = 0;
+
+  const incrementCount = () => {
+    count += 1;
+  };
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Counter {count}!</h1>
+  <button on:click={incrementCount}>Add</button>
+</main>
